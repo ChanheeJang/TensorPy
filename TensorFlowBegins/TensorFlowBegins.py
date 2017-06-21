527b15d8a8ccd4dd9e490e872e09e74df04798f6
ChanheeJang <chan@ati2000.co.kr>
2017-06-21 19:23:57 +0900

    test4

--- TensorFlowBegins/TensorFlowBegins.py
+++ TensorFlowBegins/TensorFlowBegins.py
@@ -1,3 +1,8 @@
+giant panda, panda, panda bear, coon bear, Ailuropoda melanoleuca (score = 0.88493)
+indri, indris, Indri indri, Indri brevicaudatus (score = 0.00878)
+lesser panda, red panda, panda, bear cat, cat bear, Ailurus fulgens (score = 0.00317)
+custard apple (score = 0.00149)
+earthstar (score = 0.00127)
  // Now try to figure out what kind of file it is and decode it.
   const int wanted_channels = 3;
   tensorflow::Node* image_reader;
