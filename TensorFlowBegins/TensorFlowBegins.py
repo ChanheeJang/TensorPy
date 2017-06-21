be266d2bfe7d3c8f2280ef263c56f8c12ff28fa7
ChanheeJang <chan@ati2000.co.kr>
2017-06-21 19:27:44 +0900

    Update TensorFlowBegins.py

--- TensorFlowBegins/TensorFlowBegins.py
+++ TensorFlowBegins/TensorFlowBegins.py
@@ -5,32 +5,10 @@ custard apple (score = 0.00149)
 earthstar (score = 0.00127)
  // Now try to figure out what kind of file it is and decode it.
   const int wanted_channels = 3;
-  tensorflow::Node* image_reader;
-  if (tensorflow::StringPiece(file_name).ends_with(".png")) {
-    image_reader = tensorflow::ops::DecodePng(
-        file_reader,
-        b.opts().WithAttr("channels", wanted_channels).WithName("png_reader"));
-  } else {
-    // Assume if it's not a PNG then it must be a JPEG.
-    image_reader = tensorflow::ops::DecodeJpeg(
-        file_reader,
-        b.opts().WithAttr("channels", wanted_channels).WithName("jpeg_reader"));
-  }
-  // Now cast the image data to float so we can do normal math on it.
-  tensorflow::Node* float_caster = tensorflow::ops::Cast(
-      image_reader, tensorflow::DT_FLOAT, b.opts().WithName("float_caster"));
-  // The convention for image ops in TensorFlow is that all images are expected
-  // to be in batches, so that they're four-dimensional arrays with indices of
-  // [batch, height, width, channel]. Because we only have a single image, we
-  // have to add a batch dimension of 1 to the start with ExpandDims().
-  tensorflow::Node* dims_expander = tensorflow::ops::ExpandDims(
-      float_caster, tensorflow::ops::Const(0, b.opts()), b.opts());
-  // Bilinearly resize the image to fit the required dimensions.
-  tensorflow::Node* resized = tensorflow::ops::ResizeBilinear(
-      dims_expander, tensorflow::ops::Const({input_height, input_width},
-                                            b.opts().WithName("size")),
-      b.opts());
-  // Subtract the mean and divide by the scale.
+  tensorflow::Node* image_reader
+    dddddddddddddddddddddddddddddddddd
+    ddddddddddddddddddddddddddddddddddd
+    ddddddddddddddddddddddddddddddddddddd
   tensorflow::ops::Div(
       tensorflow::ops::Sub(
           resized, tensorflow::ops::Const({input_mean}, b.opts()), b.opts()),
