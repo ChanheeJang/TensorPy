9e8202932cb25a81f169185f43338a9c3a663f92
ChanheeJang <chan@ati2000.co.kr>
2017-06-27 19:48:24 +0900

    viewGraph

--- TensorFlowBegins/TensorFlowBegins.py
+++ TensorFlowBegins/TensorFlowBegins.py
@@ -1,16 +1,17 @@
-giant panda, panda, panda bear, coon bear, Ailuropoda melanoleuca (score = 0.88493)
-indri, indris, Indri indri, Indri brevicaudatus (score = 0.00878)
-lesser panda, red panda, panda, bear cat, cat bear, Ailurus fulgens (score = 0.00317)
-custard apple (score = 0.00149)
-earthstar (score = 0.00127)
- // Now try to figure out what kind of file it is and decode it.
-  const int wanted_channels = 3;
-  tensorflow::Node* image_reader
-    dddddddddddddddddddddddddddddddddd
-    ddddddddddddddddddddddddddddddddddd
-    ddddddddddddddddddddddddddddddddddddd
-  tensorflow::ops::Div(
-      tensorflow::ops::Sub(
-          resized, tensorflow::ops::Const({input_mean}, b.opts()), b.opts()),
-      tensorflow::ops::Const({input_std}, b.opts()),
-      b.opts().WithName(output_name));
+﻿import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+
+import tensorflow as tf
+
+a=tf.constant(2)
+b=tf.constant(3)
+
+x=tf.add(a,b)
+
+with tf.Session() as sess:
+    
+    writer = tf.summary.FileWriter('./graphs',sess.graph)
+    print( sess.run(x))
+
+writer.close()
